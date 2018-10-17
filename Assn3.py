c0a601cce08feb5ec74093581cdf818642b8112d
Mythreya K <mythreya.github@gmail.com>
2018-10-18 02:20:59 +0530

    Fixed a few semantic bugs, added features

---
+++ Assn3.py
@@ -0,0 +1,51 @@
+from DecisionTree import DecisionTree
+import time
+from multiprocessing import Pool
+
+
+def func(skp):
+    Des = DecisionTree("datasets\\Sensorless_drive_diagnosis.csv")
+    # Des = DecisionTree("datasets\\sensor_truncated.csv")
+    Des.Config(skipRatio=skp[0], maxDepth=skp[1])
+
+    # Training times
+    trSt = time.time()
+    Des.Train()
+    trEnd = time.time()
+    evlTime = trEnd-trSt
+
+    # Testing times
+    tstSt = time.time()
+    res = Des.Test(Des.rootData[:, :-1])
+    tstEnd = time.time()
+    tstTime = tstEnd - tstSt
+
+    # Accuracy
+    correct = 0
+    wrong = 0
+    for i in range(len(res)):
+        if res[i] == Des.rootData[:, -1][i]:
+            correct += 1
+        else:
+            wrong += 1
+    accr = correct/(correct+wrong)*100
+
+    # Sort and check times
+    sortTime = sum(Des.srtTimes)
+    chkTime = sum(Des.chkTimes)
+    totTime = sortTime + chkTime
+
+    # Time errors
+    delta = totTime - evlTime
+
+    print(  "Skips:"        + str(skp[0]) +
+          "  MaxDepth:"     + str(skp[1]) +
+          "  Accu:"         + str.format("{0:5.3f}" ,  accr) +
+          "  TotTime:"      + str.format("{0:9.3f}" ,  Des.TrainTime) +
+          "  (+/-)"         + str.format("{0: 6.3f}",  delta) +
+          "  Sort %:"       + str.format("{0:7.3f}" ,  (sortTime/evlTime)*100) +
+          "  Entr Time %:"  + str.format("{0:7.3f}" ,  (chkTime/evlTime)*100) +
+          "  Test Time:"    + str.format("{0:6.3f}" ,  tstTime))
+
+if __name__ == "__main__":
+    func([0.035, 30])
--- DecisionTree.py
+++ DecisionTree.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import random as rd
 import math
@@ -6,111 +5,150 @@ import matplotlib.pyplot as plt
 import sys
 import time
 
+
 class Node:
     def __init__(self, nodeData):
         self.dataset = nodeData
         self.left = None
         self.right = None
-        # Values used during testing 
+        # Values used during testing
         self.splitVal = None
         self.splitAttr = None
         self.cls = None
+
 
 class DecisionTree:
     def __init__(self, dataset):
         self.rootData = self._readCSV(dataset)
         self.noOfAttr = self.rootData.shape[1] - 1
-        self.Tree = Node(self.rootData)        
+        self.Tree = Node(self.rootData)
         self.rootNode = Node(self.rootData)
 
         # Configure the hyperparameters
         self.skips = None
         self.maxDepth = None
-        self.Config()
 
-    def Config(self, skips = 0.3):
+        if self.rootData.shape[0] < 1000:
+            self.Config(skipRatio=1)
+        elif self.rootData.shape[0] < 10000:
+            self.Config(skipRatio=0.5, maxDepth=int(self.rootData.shape[1]*0.7))
+        else:
+            self.Config(
+                skipRatio=0.01, 
+                maxDepth=int(self.rootData.shape[1]*0.5) 
+            )
+        
+        # Performance Metrics
+        self.srtTimes = []
+        self.chkTimes = []
+        self.DReadT = 0
+        self.TrainTime = 0
+
+    def Config(self, skipRatio=1, maxDepth=np.Inf):
         # Hyperparameters
-        self.skips = skips
-        self.maxDepth = np.Inf
+        if skipRatio ==1 :
+            raise ValueError("Skip Ratio /must be a value in the range (0, 1]  ")
+        elif skipRatio > 0.5:
+            print("Warning! Setting skip ratio above 0.5 is the same as setting it to 1!")
+            time.sleep(5)
+            print("Continuing...")
+        
+        self.skips = int(1/skipRatio)          # Given a percentage, calculate actual skips 
+        self.maxDepth = maxDepth
 
     def Train(self):
+        a = time.time()
         self._buildTree(self.rootNode, 0)
+        self.TrainTime = time.time() - a
 
     def _buildTree(self, node, currentDepth):
-        if currentDepth < self.maxDepth:  
-        
-            # If the node has only one type of class objects, assign class and return        
-            if len(np.unique(node.dataset[:, -1])) == 1 :
+        if currentDepth < self.maxDepth:
+
+            # If the node has only one type of class objects, assign class and return
+            if len(np.unique(node.dataset[:, -1])) == 1:
                 node.cls = self._setClass(node)
                 return
             else:
-                leftData, rightData, relativeAttrToSplitOn = self._getBestSplit(node)
+                leftData, rightData, relativeAttrToSplitOn, valueToSplitOn = self._getBestSplit(node)
                 if (leftData is not None) and (rightData is not None):
-                    node.left  = Node(leftData)
-                    node.right = Node(rightData) 
-                    # A node's attr to split on only takes the relative index: 
-                    # If root node has attrs to split on as 0 1 2 3 4 5 6, some node might get 
-                    # 0 4 5 6 . If this node splits on '4', it is actually stored as '1'. 
-                    node.splitAttr = relativeAttrToSplitOn      
-                    self._buildTree(node.left,  currentDepth +1)
-                    self._buildTree(node.right, currentDepth +1)
+                    node.left = Node(leftData)
+                    node.right = Node(rightData)
+                    # A node's attr to split on only takes the relative index:
+                    # If root node has attrs to split on as 0 1 2 3 4 5 6, some node might get
+                    # 0 4 5 6 . If this node splits on '4', it is actually stored as '1'.
+                    node.splitAttr = relativeAttrToSplitOn
+                    node.splitVal = valueToSplitOn
+                    self._buildTree(node.left,  currentDepth + 1)
+                    self._buildTree(node.right, currentDepth + 1)
                 else:
-                    # This node cannot be split further, so assign a class and return 
+                    # This node cannot be split further, so assign a class and return
                     node.cls = self._setClass(node)
         else:
-            # Max depth reached. Assign class and return 
+            # Max depth reached. Assign class and return
             node.cls = self._setClass(node)
             return
 
-
     def _setClass(self, node):
-        u, c = np.unique(node.dataset[:,-1], return_counts=True) 
+        u, c = np.unique(node.dataset[:, -1], return_counts=True)
         return node.dataset[:, -1][np.argmax(c)]
 
     def _getBestSplit(self, node):
-        # If the number of rows received is (0 or 1) OR columns is 1 (i.e. has only classes), cannot split, so return None 
+        # If the number of rows received is (0 or 1) OR columns is 1 (i.e. has only classes), cannot split, so return None
         if (node.dataset.shape[0] <= 1) or (node.dataset.shape[1] == 1):
-                return None, None, None
-        # Get the best split and also 
+            return None, None, None, None
+        # Get the best split and also
         # assign the node.splitval to the best split condition
         splitsInxs = []
         entr = []
-        
+
         # Iterate over the attributes and get the best split value for each chosen attribute
-        for attr in range(node.dataset.shape[1] - 1):       # Ignore the last column, has the classes
-            spltPointInx, entropyGen = self._getBestSplitVal(node.dataset[:, [attr,-1]])
+        # Ignore the last column, has the classes
+        for attr in range(node.dataset.shape[1] - 1):
+            spltPointInx, entropyGen = self._getBestSplitVal(
+                node.dataset[:, [attr, -1]])
             splitsInxs.append(spltPointInx)
             entr.append(entropyGen)
-        
-        attrToSplitOnInx    = entr.index(min(entr))
-        valToSplitOnInx     = splitsInxs[attrToSplitOnInx]      # Get the index where an attr gives min entropy
-        
+
+        attrToSplitOnInx = entr.index(min(entr))      # Get the index where an attr gives min entropy
+        valToSplitOnInx = splitsInxs[attrToSplitOnInx]
+
         v = valToSplitOnInx                                     # for bervity
         a = attrToSplitOnInx                                    # for bervity
         # sort array with the attr to split on, slice away
         srtdArr = node.dataset[node.dataset[:, a].argsort()][::-1]
-        node.splitVal = srtdArr[v][a]
-        left  = np.concatenate( (  srtdArr[v:, :a], srtdArr[v:, a+1: ]  ), axis=1)
-        right = np.concatenate( (  srtdArr[:v, :a], srtdArr[:v, a+1: ]  ), axis=1)
+        splV = srtdArr[v][a]
+        left  = np.concatenate((srtdArr[v:, :a], srtdArr[v:, a+1: ]), axis=1)
+        right = np.concatenate((srtdArr[:v, :a], srtdArr[:v, a+1: ]), axis=1)
         # get left, right, delete column, return
+        
         # But before returning, check if either one of the left/right has zero rows, meaning no split was done
-        if (left.shape[0] == 0) or (right.shape[0] == 0): 
-            return None, None, None
+        if (left.shape[0] == 0) or (right.shape[0] == 0):
+            return None, None, None, None
         else:
-            return left, right, attrToSplitOnInx
+            return left, right, attrToSplitOnInx, splV
 
     def _getBestSplitVal(self, arr):
-        # We now have a Kx2 array, where the last col = class a row belongs to 
-        # Sort the array and check entropy for the top and bottom splits, 
-        # return the val and entropy of the least entropy gen. 
+        # We now have a Kx2 array, where the last col = class a row belongs to
+        # Sort the array and check entropy for the top and bottom splits,
+        # return the val and entropy of the least entropy gen.
         minEntropys = []
-        srtdArr = arr[arr[:,0].argsort()][::-1]      # Sort w.r.t. the attribute vals, in dec order to get <= while slicing
+        srtStart = time.time()
+        srtdArr = arr[arr[:,0].argsort()][::-1]     # Sort w.r.t. the attribute vals, in dec order to get <= while slicing
+        srtEnd = time.time()
         
-        for i in range(arr.shape[0]):
-            minEntropys.append( (srtdArr[:i].shape[0]/srtdArr.shape[0]) * self._getEntropy(srtdArr[:i][:,-1] ) + 
-                                (srtdArr[i:].shape[0]/srtdArr.shape[0]) * self._getEntropy(srtdArr[i:][:,-1] )  )
+        self.srtTimes.append(srtEnd-srtStart)
+        # For better speed at the cost of accuracy, we skip the entropy check for a few rows
+        # and instead check the entropy for every P rows, P = hyperparameter.
+        # If P = 0.3, then 30% of the rows are not checked.
+        chkSt = time.time()
+        for i in range(0, arr.shape[0], self.skips):
+            minEntropys.append((srtdArr[:i].shape[0]/srtdArr.shape[0]) * self._getEntropy(srtdArr[:i][:, -1]) +
+                               (srtdArr[i:].shape[0]/srtdArr.shape[0]) * self._getEntropy(srtdArr[i:][:, -1]))
+        chkEnd = time.time()
+        self.chkTimes.append(chkEnd-chkSt)
+
         inxOfMinEnt = minEntropys.index(min(minEntropys))
-        return inxOfMinEnt, minEntropys[inxOfMinEnt]
+        return inxOfMinEnt*self.skips, minEntropys[inxOfMinEnt]
 
     def _getEntropy(self, colArray):
         # Given a column vector, gets the scalar entropy
@@ -121,7 +159,7 @@ class DecisionTree:
     def Test(self, testArr):
         if len(testArr.shape) != 2:
             raise ValueError("Takes 2D np.array types with with each row as test data")
-        # Value is a np array containing the attributes and the values. 
+        # Value is a np array containing the attributes and the values.
         # Iterate over the number of test cases, for each getting the class it belongs to
         retArr = np.zeros(testArr.shape[0])
         for i in range(testArr.shape[0]):
@@ -134,18 +172,16 @@ class DecisionTree:
     def _searchTree(self, node, arr):
         if node.splitVal is not None:
             v = node.splitAttr
-            if ( arr[v] <= node.splitVal ):
+            if arr[v] <= node.splitVal:
                 return self._searchTree(node.left,  np.concatenate(( arr[:v], arr[v+1: ])) )
             else:
                 return self._searchTree(node.right, np.concatenate(( arr[:v], arr[v+1: ])) )
         else:
             return node.cls
 
-    def _readCSV(self, datasetPath):  
-        return np.genfromtxt(datasetPath, dtype=float, delimiter=",")
-
-
-if __name__ == "__main__":
-    Des = DecisionTree("datasets\\banknote_auth.csv")
-    Des.Train()
-    print("Done!")
+    def _readCSV(self, datasetPath):
+        st = time.time()
+        arr = np.genfromtxt(datasetPath, dtype=float, delimiter=",")
+        self.DReadT = time.time() - st
+        print("Data read to memory in ", str.format( "{0:5.3}", self.DReadT) )
+        return arr
